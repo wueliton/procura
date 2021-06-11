dd8916a84cd545a1b624b8d7e3fc6391ae90bbe6
Paulo Wuéliton <paulo.wueliton@hotmail.com>
2021-06-11 18:03:20 -0300

    fix: ssl issue on get googleapi.Class.php att

--- index.php
+++ index.php
@@ -41,6 +41,8 @@
 		$urlFile = "https://raw.githubusercontent.com/wueliton/procura/master/classes/googleapi.Class.php";
 		$ch = curl_init($urlFile);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
 		$content = curl_exec($ch);
 		if(curl_errno($ch)) {
 			echo('Erro ao tentar atualizar arquivo.');
